1a8a8d8339cd531ec41a3a8955252cb9605854ea
Andra Veraart <averaart@hotmail.com>
2012-07-16 16:03:15 +0200

    Header now responds to different viewport sizes.

--- js/script.js
+++ js/script.js
@@ -6,5 +6,50 @@
 
 $(document).ready(function() {
 	$('h2').textfill({ maxFontPixels: 60 });
+	
+	$(window).resize(function() {
+		var viewportWidth = $(window).width();
+		var headerPadding = parseInt($('header').css('padding-left').replace("px", ""));
+		var logoWidth = $('#logo').width();
+		
+		if (
+			(viewportWidth < 470+(headerPadding*2))
+			&&
+			(logoWidth <= 470)
+			){
+				var newWidth = viewportWidth-(headerPadding*2)
+				$('#logo').width(newWidth);
+				$('#logo').height(72/470*newWidth);
+			} else if (logoWidth < 470){
+				$('#logo').width(470);
+				$('#logo').height(72);
+			}
+		
+		
+		
+// 		if	(
+// 				(viewportWidth < 470+(headerPadding*2))
+// 				&&
+// 				(logoWidth == 470)
+// 			){
+// 			console.log("kleiner");
+// 		console.log(viewportWidth);
+// 		console.log(headerPadding);
+// 		console.log(logoWidth);
+// 			$('#logo').width(220);
+// 		} else if(
+// 				(viewportWidth > 470+(headerPadding*2))
+// 				&&
+// 				(logoWidth == 220)
+// 			){
+// 			console.log("groter");
+// 		console.log(viewportWidth);
+// 		console.log(headerPadding);
+// 		console.log(logoWidth);
+// 			$('#logo').width(470);
+// 		}
+	});
+	
+	$(window).resize();
 });
 
