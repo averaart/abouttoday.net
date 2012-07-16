c3a341b5f09984c0f9ee9595736e9915022af52c
Andra Veraart <averaart@hotmail.com>
2012-07-17 00:46:11 +0200

    Images are pulled from a Flickr set and displayed in a colorbox.

--- js/script.js
+++ js/script.js
@@ -31,6 +31,7 @@ $(document).ready(function() {
 	var today = now.format("yyyy-mm-dd");
 	var yearAgo = new Date(now.getTime() - 365*24*60*60*1000).format("yyyy-mm-dd");
 	loadGigs("past-gigs", yearAgo+","+today);
+	loadFlickrSet("72157624826648990", "photolist");
 	
 });
 
@@ -65,4 +66,28 @@ var loadGigs = function(target, range){
 			html += "</ul>";
 			$('#'+target).html(html);		
 		});
+}
+
+var loadFlickrSet = function(setId, target){
+	var apiKey = "3d8a7e8f34196d45f383edd8d7a31c1a";
+	var request = 'http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key='+apiKey+'&photoset_id='+setId+'&extras=url_l%2C+url_sq&format=json&jsoncallback=?&per_page=9';
+	$.getJSON(request, function(response) {
+		var photos = response.photoset.photo;
+		var photo;
+		var html = "";
+		for (var i in photos){
+			photo = photos[i];
+			html += "<a href='"+photo.url_l+"' title='"+photo.title+"'>"+
+					"<img src='"+photo.url_sq+"' />"+
+					"</a>";
+		}
+		$("#"+target).html(html);
+		$("#"+target+" a").colorbox({	transition: 'elastic',
+										speed: 500,
+										rel:target,
+										maxWidth:'100%',
+										maxHeight:'100%',
+										slideshow: true,
+										slideshowSpeed: 5000});
+	});			
 }
